937625fc0893da178f485325584799cf0ea03cb9
BrandonToapanta <110624649+BrandonToapanta@users.noreply.github.com>
2024-04-26 00:32:24 -0500

    Actualizar buscar.php
    Agregar límite en búsquedas

--- buscar.php
+++ buscar.php
@@ -21,25 +21,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $id = $_GET['id'];
 
   if (!empty($nombre) && !empty($id)) {
-    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE id LIKE :id OR emp_nombres LIKE :emp_nombres");
+    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE id LIKE :id OR emp_nombres LIKE :emp_nombres LIMIT 20");
     $sql->bindValue(':id', '%' . $_GET['id'] . '%');
     $sql->bindValue(':emp_nombres', '%' . $_GET['emp_nombres'] . '%');
   } elseif (!empty($id)&& empty($nombre)) {
-    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE id LIKE :id");
+    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE id LIKE :id LIMIT 20");
     $sql->bindValue(':id', '%' . $_GET['id'] . '%');
   } elseif (empty($id) && !empty($nombre)) {
-    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE emp_nombres LIKE :emp_nombres");
+    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado WHERE emp_nombres LIKE :emp_nombres LIMIT 20");
     $sql->bindValue(':emp_nombres', '%' . $_GET['emp_nombres'] . '%');
   }
    else {
-    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado");
+    $sql = $dbConn->prepare("SELECT id, emp_nombres, emp_apellidos, emp_status FROM empleado LIMIT 20");
   }
 
-  $sql->execute(); // Ejecuta la consulta preparada
-  $sql->setFetchMode(PDO::FETCH_ASSOC); // Establece el modo de recuperación de datos
-  $resultado = $sql->fetchAll(); // Recupera los datos de la consulta
+  $sql->execute();
+  $sql->setFetchMode(PDO::FETCH_ASSOC);
+  $resultado = $sql->fetchAll();
 
   header("HTTP/1.1 200 OK");
-  echo json_encode($resultado); // Envía los datos como JSON
+  echo json_encode($resultado);
   exit();
 }
